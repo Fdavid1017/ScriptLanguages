2af40287fa345c1f8bf06ef90a179c7090dd587b
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2020-11-24 15:28:23 +0100

    added sources and A* infos side panel

--- PathFinding/static/js/functions.js
+++ PathFinding/static/js/functions.js
@@ -387,3 +387,22 @@ async function RevealCheckedCells(table) {
         await sleep(getSpeedRange())
     }
 }
+
+var navOpened = false;
+
+function navControl() {
+    if (navOpened) {
+        closeNav();
+    } else {
+        openNav();
+    }
+    navOpened = !navOpened;
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
